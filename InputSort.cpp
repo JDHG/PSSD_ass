6ef47d31337b0cefa87f580e7e683b9e895e2f39
Tommodingee <41982396+Tommodingee@users.noreply.github.com>
2019-10-16 15:31:02 +1030

    Almost functioning input collector

--- InputSort.cpp
+++ InputSort.cpp
@@ -5,14 +5,55 @@
 #include <deque>
 #include <map>
 
+using namespace std;
+
 class InputSort
 {
     bool debug;
     struct Teacher
     {
         string name;
-        vector<int> n_courses;
+        int n_courses;
+        vector<string> courses;
+        vector<int> preferences;
         Teacher(string input_name) { name = input_name; }
+        void print()
+        {
+            cout << "name :: " << name << endl;
+            cout << "courses\n{ ";
+            for(auto e : courses)
+                cout << e << " ";
+            cout << "}" << endl;
+            cout << "preferences\n{ ";
+            int day = 0;
+            int c = 8;
+            for(auto e : preferences)
+            {
+                if(!(c % 8))
+                {
+                    cout << "\nDay (" << day << ") ";
+                    day++;
+                }
+                c++;
+                cout << e << " ";
+            }
+            cout << "\n}\n" << endl;
+        }
+    };
+    //
+    struct Course
+    {
+        string name;
+        vector<int> n_teachers;
+        vector<string> teachers;
+        Course(string input_name) { name = input_name; }
+
+        void print()
+        {cout << "name :: " << name << endl;
+        cout << "teachers\n{ ";
+        for(auto e : teachers)
+            cout << e << " ";
+        cout << "}\n" << endl;}
     };
 
 
@@ -20,8 +61,11 @@ class InputSort
     int n_courses;
     int n_teachers;
     vector<int> hours;
-    vector<string> courses;
-    map<string, vector<string>> teachers; //key is teacher name, value is n_courses taught
+    vector<Course> courses;
+    vector<Teacher> teachers;
+    // deque<Teacher*> teachers;
+    // deque<Course*> courses;
+    // map<string, vector<Teacher>> teachers; //key is teacher name, value is the courses taught by teacher
 
     deque<string> * raw_input;
 
@@ -46,20 +90,20 @@ class InputSort
         raw_input = input_lines;
         return;
     }
-    vector<int> csv_to_int_vector(string csv, string separator)
+    vector<int> csv_binary_to_int_vector(string csv, string separator)
     {
-        if(debug) cout << "InputSort Construction :: csv_to_int_vector ( " << separator << " | " << csv << ")" << endl;
-        if(csv.front() != '0' || csv.front() != '1') { cout << "FATAL -> csv_to_int_vector (with separator '" << separator << "') with incorrect string" << endl; exit(1); }
+        if(debug) cout << "InputSort Construction :: csv_binary_to_int_vector ( " << separator << " | " << csv << ")" << endl;
+        // if(csv.front() != '0' || csv.front() != '1') { cout << "FATAL -> csv_to_int_vector (with separator '" << separator << "') with incorrect string" << endl; exit(1); }
         vector<int> ints = {};
-        string integer;
+        string temp = "";
         while(csv.size() > 0)
         {
-            while(isdigit(csv.front())) { integer = integer + csv.front(); csv.erase(0, 1); }
-
-            ints.push_back(stoi(integer));
-            integer = "";
+            temp += csv.front();
+            csv.erase(0, 1);
+            ints.push_back(stoi(temp));
+            temp = "";
             if(csv.size() <= 0) break;
-            if(csv.substr(0, separator.size()) != separator) { cout << "FATAL -> csv_to_int_vector" << endl; exit(1); }
+            for(int i = 0; i < separator.size(); i++) csv.erase(0, 1);
         }
 
         if(debug)
@@ -137,24 +181,35 @@ class InputSort
         return names;
     }
 
-public:
 
+public:
+    void print_teachers()
+    {
+        if(debug) cout << "\n\nInputSort print_teachers :: printing teachers\n" << endl;
+        for(auto teacher : teachers) if(debug) teacher.print();
+    }
+    void print_courses()
+    {
+        if(debug) cout << "\n\nInputSort print_courses :: printing courses\n" << endl;
+        for(auto course : courses) if(debug) course.print();
+    }
     InputSort(bool d, char const * file_name)
     {
 
         string * string_buffer = new string;
         vector<string> * strings_buffer = new vector<string>;
-        vector<int> * courses_matrix_buffer = new vector<int>;
+        vector<int> * ints_buffer = new vector<int>;
+        vector<string> * teacher_names = new vector<string>;
 
         debug = d;
         //Open file and set raw_input
         set_raw_input(file_name);
 
         //ROOMS
-        if(debug) cout << "InputSort Construction :: setting rooms      :: (" << raw_input->front().back() << ")" << endl;
+        if(debug) cout << "InputSort Construction :: setting rooms          :: (" << raw_input->front().back() << ")" << endl;
         rooms = raw_input->front().back() - '0';            raw_input->pop_front();
         //n_courses
-        if(debug) cout << "InputSort Construction :: setting n_courses    :: (" << raw_input->front().back() << ")" << endl;
+        if(debug) cout << "InputSort Construction :: setting n_courses      :: (" << raw_input->front().back() << ")" << endl;
         n_courses = raw_input->front().back() - '0';          raw_input->pop_front();
 
         //HOURS
@@ -162,29 +217,60 @@ public:
         if(string_buffer->substr(0, 12) != "Hours       ") { if(debug) cout << "FATAL -> Hours input sorting failed" << endl; exit(1); }
         if(debug) cout << "erasing (" << string_buffer->substr(0, 12) << ")" << endl;
         string_buffer->erase(0, 12);
-        if(debug) cout << "InputSort Construction :: setting hours      :: (" << *string_buffer << ")" << endl;
+        if(debug) cout << "InputSort Construction :: setting hours          :: (" << *string_buffer << ")" << endl;
         hours = csv_to_int_vector(*string_buffer);
 
-        //NAMES
+        //COURSES
         *string_buffer = raw_input->front(); raw_input->pop_front();
         if(string_buffer->substr(0, 5) != "Names") { if(debug) cout << "FATAL -> Names input sorting failed" << endl; exit(1); }
         if(debug) cout << "erasing (" << string_buffer->substr(0, 12) << ")" << endl;
         string_buffer->erase(0, 12);
-        if(debug) cout << "InputSort Construction :: setting names      :: (" << *string_buffer << ")" << endl;
-        courses = csv_to_string_vector(*string_buffer);
+        if(debug) cout << "InputSort Construction :: setting names          :: (" << *string_buffer << ")" << endl;
+        *strings_buffer = csv_to_string_vector(*string_buffer);
+        for(auto course_name : *strings_buffer)
+            courses.push_back(Course(course_name));
+
 
         //N_TEACHERS
-        if(debug) cout << "InputSort Construction :: setting n_teachers :: (" << raw_input->front().back() << ")" << endl;
+        if(debug) cout << "InputSort Construction :: setting n_teachers     :: (" << raw_input->front().back() << ")" << endl;
         n_teachers = raw_input->front().back() - '0';       raw_input->pop_front();
 
         //TEACHERS
         *string_buffer = raw_input->front();                raw_input->pop_front();
-        if(debug) cout << "InputSort Construction :: setting Teachers   :: (" << *string_buffer << ")" << endl;
+        if(debug) cout << "InputSort Construction :: setting Teachers       :: (" << *string_buffer << ")" << endl;
         *strings_buffer = csv_to_string_vector(*string_buffer);
+        for(int i = 0; i < strings_buffer->size(); i++)
+            teachers.push_back(Teacher(strings_buffer->at(i)));
 
-    
+        //TEACHER SUBJECTS INPUTTING
+        *string_buffer = raw_input->front();                raw_input->pop_front();
+        if(string_buffer->substr(0, 3) != "%TL") { cout << "FATAL -> TL courses teacher matrix out of sync" << endl; exit(1); }
+        for(int i = 0; i < n_courses; i++)
+        {
+            *string_buffer = raw_input->front();                raw_input->pop_front();
+            *ints_buffer = csv_binary_to_int_vector(*string_buffer, ",");
 
+            for(int j = 0; j < n_teachers; j++)
+                if(ints_buffer->at(j) == 1)
+                {
+                    courses.at(i).teachers.push_back(teachers.at(j).name);
+                    teachers.at(j).courses.push_back(courses.at(i).name);
+                }
 
+        }
+
+        //TEACHER PREFERENCES INPUTTING
+        *string_buffer = raw_input->front();                raw_input->pop_front();
+        if(string_buffer->substr(0, 3) != "%LP") { cout << "FATAL -> LP preferences out of sync" << endl; exit(1); }
+        for(int i = 0; i < n_teachers; i++)
+        {
+            *string_buffer = raw_input->front();            raw_input->pop_front();
+            *ints_buffer = csv_binary_to_int_vector(*string_buffer, ",");
+            teachers.at(i).preferences = *ints_buffer;
+        }
+
+        print_teachers();
+        print_courses();
 
 
         // while(!strings_buffer->empty())
@@ -195,6 +281,9 @@ public:
 
         delete string_buffer;
         delete strings_buffer;
+        delete ints_buffer;
+        delete teacher_names;
+
 
     }
 
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include "InputSort.cpp"
+
+using namespace std;
+
+//Arguments to main are <debug> <file_name>
+
+int main(int argc, char const * argv[]) {
+
+    if(argc < 3) cout << "FATAL -> not enough arguments on main call (" << argc << "arguments)" << endl;
+    if(argc > 3) cout <<   "FATAL -> too many arguments on main call (" << argc << "arguments)" << endl;
+
+    bool debug = false;
+    string * do_debug = new string; *do_debug = argv[1];
+    if(*do_debug == "true") debug = true;
+    delete do_debug;
+    char const * file_name = argv[2];
+
+    if(debug) cout << "Initiating timetable booking with input file :: '" << file_name << "'" << endl;
+    if(debug) cout << "Debug is TRUE (" << debug << ")" << endl;
+
+    InputSort input = InputSort(debug, file_name);
+
+    return 0;
+}
